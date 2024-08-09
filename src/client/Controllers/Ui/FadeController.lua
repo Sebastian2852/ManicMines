8fad47ca9652e5152bce840ce16536326f8f2575
Sebastian <sebastian2852@proton.me>
2024-08-09 05:14:16 +0100

    Fade Controller

---
+++ src/client/Controllers/Ui/FadeController.lua
@@ -0,0 +1,100 @@
+local Knit = require(game.ReplicatedStorage.Packages.Knit)
+
+local TweenService = game:GetService("TweenService")
+
+local FadeController = Knit.CreateController {
+    Name = "FadeController"
+}
+
+local Player = game.Players.LocalPlayer
+local FadeFrame :Frame = nil
+local FadeTweenInfo :TweenInfo = nil
+
+--[[ INTERNAL ]]--
+
+local function FadeGuiIn(Animated :boolean, AwaitFinish :boolean)
+    if Animated then
+        local Fade = TweenService:Create(FadeFrame, FadeTweenInfo, {BackgroundTransparency = 0})
+        Fade:Play()
+
+        if AwaitFinish then 
+            Fade.Completed:Wait() 
+        end
+    else
+        FadeFrame.BackgroundTransparency = 0
+    end
+end
+
+local function FadeGuiOut(Animated :boolean, AwaitFinish :boolean)
+    if Animated then
+        local Fade = TweenService:Create(FadeFrame, FadeTweenInfo, {BackgroundTransparency = 1})
+        Fade:Play()
+
+        if AwaitFinish then 
+            Fade.Completed:Wait() 
+        end
+    else
+        FadeFrame.BackgroundTransparency = 1
+    end
+end
+
+
+
+--[[ FUNCTIONS ]]--
+
+--[[
+Fades in a black GUI to cover the whole screen and any UI
+if await for finish is true then the script will not return
+until the fade has finished
+]]--
+function FadeController:FadeGameplayOut(AwaitFinish :boolean)
+    FadeGuiIn(true, AwaitFinish)
+end
+
+--[[
+Fades out the GUI if await for finish is true then the script
+will not return until the fade has finished
+]]--
+function FadeController:FadeGameplayIn(AwaitFinish :boolean)
+    FadeGuiOut(true, AwaitFinish)
+end
+
+--[[
+Puts the black gui on the screen with no fade/animation
+]]--
+function FadeController:HideGameplay()
+    FadeGuiIn(false, false)
+end
+
+--[[
+gets rid of the black gui on the screen with no fade/animation
+]]--
+function FadeController:ShowGameplay()
+    FadeGuiOut(false, false)
+end
+
+
+
+--[[ KNIT ]]--
+
+function FadeController:KnitInit()
+    local NewScreenGui = Instance.new("ScreenGui")
+    NewScreenGui.Parent = Player.PlayerGui
+    NewScreenGui.Name = "FadeGui"
+    NewScreenGui.IgnoreGuiInset = true
+    NewScreenGui.ClipToDeviceSafeArea = false
+    NewScreenGui.ResetOnSpawn = false
+    NewScreenGui.DisplayOrder = 100
+
+    local NewFrame = Instance.new("Frame")
+    NewFrame.Parent = NewScreenGui
+    NewFrame.Name = "Fade"
+    NewFrame.Size = UDim2.fromScale(1, 1)
+    NewFrame.BackgroundTransparency = 1
+    NewFrame.BackgroundColor3 = Color3.new(0, 0, 0)
+
+    FadeFrame = NewFrame
+    FadeTweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, false, 0)
+end
+
+return FadeController
---
+++ src/client/FadeTest.client.lua
@@ -0,0 +1,22 @@
+local Knit = require(game.ReplicatedStorage.Packages.Knit)
+local UserInputService = game:GetService("UserInputService")
+
+-- there has to be some other way of doing this
+-- This seems like it def has a better way of doing
+repeat
+    task.wait(0.01)
+until Knit.ControllersReady
+
+local FadeController = Knit.GetController("FadeController")
+
+UserInputService.InputBegan:Connect(function(Input)
+    if Input.KeyCode == Enum.KeyCode.Z then
+        FadeController:FadeGameplayOut(false);
+        return
+    end
+
+    if Input.KeyCode == Enum.KeyCode.X then
+        FadeController:FadeGameplayIn(false);
+        return
+    end
+end)
